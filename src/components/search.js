49c8cff3c44095190e549feaae44970cad61b273
Abdul Jaleel <mbaba56@gmail.com>
2022-06-16 07:24:54 +0400

    WeatherApi was implemented along with UI updates

---
+++ src/components/search.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    FlatList,
+    TouchableOpacity,
+    TextInput, Image
+} from 'react-native';
+
+export default function Searchbar({ value, onSubmitEditing, updateSearch, style }) {
+
+    const [query, setQuery] = useState();
+    const [error, setError] = useState()
+    return (
+        <View style={[styles.container, style]}>
+            <View style={styles.searchContainer}>
+                <View style={styles.vwSearch}>
+                    <Image
+                        style={styles.icSearch}
+                        source={require('../assets/ic_search.png')} />
+                </View>
+
+                <TextInput
+                    value={query}
+                    placeholder="Type City Name Here..."
+                    placeholderTextColor={'#000'}
+                    style={styles.textInput}
+                    onSubmitEditing={onSubmitEditing}
+                    onChangeText={(text) => {
+                        var letters = /^$|^[a-zA-Z._\b ]+$/;
+                        if (text.length > 20)
+                            setError("Search Query too long.")
+                        else if (text.match(letters)) {
+                            setQuery(text)
+                            updateSearch(text)
+                            if (error)
+                                setError(false)
+                        }
+                        else setError("Only alphabets allowed")
+                    }}
+                />
+                {
+                    query ?
+                        <TouchableOpacity
+                            onPress={() => setQuery('')}
+                            style={styles.vwClear}>
+                            <Image
+                                style={styles.icClear}
+                                source={require('../assets/ic_clear.png')} />
+                        </TouchableOpacity>
+                        : <View style={styles.vwClear} />
+                }
+
+            </View>
+            {
+                error &&
+                <Text style={styles.txtError}>
+                    {error}
+                </Text>
+            }
+        </View >
+    )
+}
+const styles = StyleSheet.create({
+    txtError: {
+        marginTop: '2%',
+        width: '95%',
+        color: 'white',
+
+    },
+    vwClear: {
+        flex: 0.2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textInput: {
+        // backgroundColor: 'green',
+        color: 'gray',
+        flex: 1,
+    },
+
+    vwSearch: {
+        flex: 0.2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // width: 40,
+        // backgroundColor: 'red'
+    },
+    icSearch: {
+        height: 18, width: 18
+    },
+    searchContainer:
+    {
+        backgroundColor: 'white',
+        width: '98%',
+        height: 40,
+        flexDirection: 'row'
+
+    },
+    container: {
+        height: 80,
+        alignItems: 'center',
+        // height: '100%', width: '100%' 
+    },
+});
